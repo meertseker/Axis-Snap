be27c3c00507cab5cd9acbec511df54df5e1441c
Senin Adın <senin_email_adresin@example.com>
2024-08-30 11:22:50 -0800

    skeleton of hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Button from "./components/Button";
 import Header from "./components/header";
+import Hero from "./components/Hero";
 const App = () => {
   return (
     <>
       <h1 className="text-3xl font-bold underline">h</h1>
       <div className="pt-[4,75] lg:pt[5.25rem] overflow-hidden">
         <Header />
+        <Hero />
       </div>
       <ButtonGradient />
     </>
