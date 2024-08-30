c0f58a69a1c2e4e8bd71d7a137d6bbb381d5bbc3
Senin Adın <senin_email_adresin@example.com>
2024-08-30 03:39:28 -0800

    navbar component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Button from "./components/Button";
+import Header from "./components/header";
 const App = () => {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <div className="pt-[4,75] lg:pt[5.25rem] overflow-hidden">
-        <Button className="mt-10" href="#login">
-          something
-        </Button>
+        <Header />
       </div>
       <ButtonGradient />
     </>
